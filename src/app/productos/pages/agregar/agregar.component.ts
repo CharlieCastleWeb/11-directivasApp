f96266efffb7f86d6113c999548685982f3c8f1e
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-11 01:16:03 +0100

    Fin proyecto directivasApp

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -10,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AgregarComponent{
 
   texto1: string = 'Fernando Herrera';
+  color: string = '';
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required]
@@ -18,11 +19,16 @@ export class AgregarComponent{
   constructor( private fb: FormBuilder) { }
 
   tieneError( campo: string): boolean {
-    return this.miFormulario.get('nombre')?.invalid || false;
+    return this.miFormulario.get(campo)?.invalid || false;
   }
 
   cambiarNombre() {
-    this.texto1 = "Juan Carlos";
+    this.texto1 = Math.random().toString();
+  }
+
+  cambiarColor() {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
  
 
